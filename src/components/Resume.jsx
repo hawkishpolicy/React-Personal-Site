bc42851901d097cde953e2acc6ffa886dfd3b2fe
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2023-07-21 15:14:08 -0400

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import resumeWithLinksPDF from "./../pdf/resumePDF-with-listed-links.docx.pdf"
+import resumeWithLinksPDF from "./../pdf/Thomas Johnson Resume w_ listed links 07:23.docx.pdf"
 
 const Resume = () => {
   return (
     <div id="resume-page">
         <iframe
           id="resume"
-          src="https://docs.google.com/document/d/e/2PACX-1vSGh0X54y9ZIXEmDyXmOYmRoJWGbdaGGCi9OX4kcU9QEf9vK3zM2GFLeWKzAQG0YA/pub?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vSoYwo3Yyjek4RIyAWhLwp0ocPKU2lSskIlAfMppiDGSVy6IB57Pfk_HATURLvcfQ/pub?embedded=true"
           ></iframe>
         <Button id="resume-button" href={resumeWithLinksPDF} target="_blank">Download</Button>
     </div>
